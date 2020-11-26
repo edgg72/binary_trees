3b6f97b10d4a7c1c7a3e9fccadaaa50bc114c304
edgg72 <edgg72@hotmail.com>
2020-11-26 14:04:25 -0500

    betty fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,18 +1,15 @@
 #include "binary_trees.h"
-
 /**
 * binary_tree_height - checks the height of a binary tree
 * @tree: the binary tree to check
 * Return: height of BT
 */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
@@ -20,13 +17,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (left + 1);
 	return (right + 1);
 }
-
 /**
 * binary_tree_size - returns the size of a BT
 * @tree: BT to check
 * Return: size of BT
 */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
@@ -34,13 +29,11 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	else
 		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
-
 /**
 * binary_tree_is_perfect - checks f a BT is perfect
 *@tree: the BT
 * Return: 1 if true, 0 otherwise
 */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (tree == NULL)
