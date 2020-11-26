9f980cb6f5d5ed402408d539109c30bbcbbc4349
edgg72 <edgg72@hotmail.com>
2020-11-26 11:28:09 -0500

    adding 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_full - checks if a BT is full
+* @tree: BT
+* Return: 1 if true, 0 otherwise
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
