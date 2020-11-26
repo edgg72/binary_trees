2810b6bc055402957786e2590f2615b11fd40b06
edgg72 <edgg72@hotmail.com>
2020-11-26 12:55:16 -0500

    adding 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - returns a pointer to the sibling of a node
+* @node: the node tocheck
+* Return: pointer to the sibling node
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
+
+    if (node->parent->left == node)
+    {
+        if (node->parent->right == NULL)
+            return (NULL);
+        else
+            return (node->parent->right);
+    }
+    else if (node->parent->right == node)
+    {
+        if (node->parent->left == NULL)
+            return (NULL);
+        else
+            return (node->parent->left);
+    }
+
+    return (NULL);
+}
