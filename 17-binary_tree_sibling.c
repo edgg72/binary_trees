5658fa64fc5b12df13601e20d428863d71f067b8
edgg72 <edgg72@hotmail.com>
2020-11-26 12:55:40 -0500

    adding 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,23 +8,23 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node->parent->left == node)
-    {
-        if (node->parent->right == NULL)
-            return (NULL);
-        else
-            return (node->parent->right);
-    }
-    else if (node->parent->right == node)
-    {
-        if (node->parent->left == NULL)
-            return (NULL);
-        else
-            return (node->parent->left);
-    }
+	if (node->parent->left == node)
+	{
+		if (node->parent->right == NULL)
+			return (NULL);
+		else
+			return (node->parent->right);
+	}
+	else if (node->parent->right == node)
+	{
+		if (node->parent->left == NULL)
+			return (NULL);
+		else
+			return (node->parent->left);
+	}
 
-    return (NULL);
+	return (NULL);
 }
